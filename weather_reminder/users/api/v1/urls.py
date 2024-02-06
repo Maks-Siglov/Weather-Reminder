1e8c0eead78aa6ef3f3a096ee2100c6d2921a68c
Maks-Siglov <gmb333x2@gmail.com>
2024-02-06 11:58:57 +0200

    Start of api/auth/v1, flake config

---
+++ weather_reminder/users/api/v1/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from weather_reminder.users.api.v1 import views
+
+urlpatterns = [
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ weather_reminder/users/api/v1/views.py
@@ -0,0 +1,39 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.api.v1.serializers import UserSerializer
+from users.models import User
+
+
+class Register(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class Login(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed(
+                f"User with with email {email} not found"
+            )
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect password")
+
+        refresh_token = RefreshToken.for_user(user)
+
+        return Response({
+            "message": "Login successful",
+            "refresh": str(refresh_token),
+            "access": str(refresh_token.access_token)
+        })
