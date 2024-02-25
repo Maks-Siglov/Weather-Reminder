352e656b4cc32a673398c54b409ed2a5e0967393
Maks-Siglov <gmb333x2@gmail.com>
2024-02-25 21:50:56 +0200

    Envs separated

--- weather_reminder/core/settings/base.py
+++ weather_reminder/core/settings/base.py
@@ -6,10 +6,6 @@ import os
 from datetime import datetime, timedelta
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-# load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
