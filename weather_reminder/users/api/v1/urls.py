23e9354c787d4b21e97d54e8e4ef342e9b847a4b
Maks-Siglov <gmb333x2@gmail.com>
2024-02-29 11:03:49 +0200

    Token bug fix

--- weather_reminder/users/api/v1/urls.py
+++ weather_reminder/users/api/v1/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from users.api.v1 import views
 
 app_name = "auth_api"
@@ -8,4 +12,6 @@ urlpatterns = [
     path("register/", views.Register.as_view(), name="register"),
     path("login/", views.Login.as_view(), name="login"),
     path("logout/", views.Logout.as_view(), name="logout"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
